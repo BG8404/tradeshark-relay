888aa99159cc9751d023a58fd52be4870c2e1119
BG8404 <williamgrello@yahoo.com>
2026-01-06 17:12:40 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "tradeshark-relay",
+  "version": "1.0.0",
+  "description": "TradingView Webhook Relay for TradeShark",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "ws": "^8.13.0",
+    "body-parser": "^1.20.2"
+  },
+  "engines": {
+    "node": "18.x"
+  }
+}
